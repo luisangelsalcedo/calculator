3e4c82a201493e1d276036b39a48b277a0a2bd5b
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-06-15 18:34:48 -0500

    💻 Publishing on github pages.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+  base: '/calculator',
   server: {
     port: 3000,
     host: true,
   },
   plugins: [react(), svgr()],
+  build: {
+    outDir: 'build',
+  },
 });
